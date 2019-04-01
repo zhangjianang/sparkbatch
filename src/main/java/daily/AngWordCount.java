0edc13349ccd834cbf9e728775f918cbb7172471
zhangja <zhangja@xiaoshouyi.com>
2019-04-01 19:12:59 +0800

    自测wordcount

---
+++ src/main/java/daily/AngWordCount.java
@@ -0,0 +1,52 @@
+package daily;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Created by adimn on 2019/4/1.
+ */
+public class AngWordCount {
+    public static void main(String[] args) throws InterruptedException {
+        SparkConf conf = new SparkConf()
+                .setMaster("local[2]")
+                .setAppName("angTry");
+        JavaStreamingContext jsc = new JavaStreamingContext(conf, Durations.seconds(5));
+        JavaDStream<String> lines = jsc.textFileStream("");
+        JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public Iterator<String> call(String instr) throws Exception {
+
+                return Arrays.asList(instr.split(" ")).iterator();
+            }
+        });
+
+        JavaPairDStream<String,Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(String s) throws Exception {
+                return new Tuple2<String, Integer>(s,1);
+            }
+        });
+
+        JavaPairDStream<String,Integer> res = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1+v2;
+            }
+        });
+        res.print();
+        jsc.start();
+        jsc.awaitTermination();
+        jsc.stop();
+    }
+}
